73ca6d8e5ed2d56c6f03c89cc97b83dbafb03b33
JordanCo1991 <68417100+JordanCo1991@users.noreply.github.com>
2023-05-07 15:38:08 +0300

    week2 first commit

---
+++ Week2/day1/exercise.py
@@ -0,0 +1,72 @@
+## Exercise 1 ##
+
+# print("Hello World\n"*3)
+
+
+## Exercise 2 ##
+
+
+# print((99**3)*8)
+
+## Exercise 3 ##
+
+# False
+# True
+# False
+# X
+# False
+
+## Exercise 4 ##
+
+# computer_brand = "Apple"
+# print(f"I have a {computer_brand} computer")
+
+
+## Exercise 5 ##
+
+# name = "Jordan"
+# age = "32"
+# shoe_size= "45"
+# info=f"Hello I'm {name}, I'm {age} and if you want to know my shoes size is {shoe_size}"
+# print(info)
+
+## Exercise 6 ##
+
+# a = 456
+# b = 340
+
+# if a >= b:
+#     print("Hello World")
+
+## Exercise 7 ##
+
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("{0} is Even".format(num))
+# else:
+#    print("{0} is Odd".format(num))
+
+
+
+## Exercise 8 ##
+
+
+# your_name = input("What's your name? ")
+
+# my_name = "Jordan"
+
+# if your_name == my_name:
+#     print("Hey, we have the same name! Its the most beautiful name in the World!")
+# else:
+#     print(f"Nice to meet you, {your_name} but my name is more beautiful than yours")
+
+## Exercise 9 ##
+height_inches = float(input("Enter your height in inches: "))
+
+
+height_cm = height_inches * 2.54
+
+if height_cm > 145:
+    print("You are tall enough to ride!")
+else:
+    print("Sorry, you need to grow some more to ride.")
