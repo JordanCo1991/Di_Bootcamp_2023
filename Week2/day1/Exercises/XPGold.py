cd55f7abec90f56b23cef952da2fa81d780bda10
JordanCo1991 <68417100+JordanCo1991@users.noreply.github.com>
2023-05-07 16:08:00 +0300

    xpgold

---
+++ Week2/day1/Exercises/XPGold.py
@@ -0,0 +1,18 @@
+## Exercise 1 ##
+# print("Hello world\n" * 4 + "I love python\n" * 4)
+
+
+## Exercise 2 ##
+
+# month = int(input("Enter a month (1 to 12): "))
+
+# if month in [3, 4, 5]:
+#     season = "Spring"
+# elif month in [6, 7, 8]:
+#     season = "Summer"
+# elif month in [9, 10, 11]:
+#     season = "Autumn"
+# else:
+#     season = "Winter"
+
+# print(f"The season of month {month} is {season}.")
