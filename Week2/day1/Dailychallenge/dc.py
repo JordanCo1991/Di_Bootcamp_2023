f23761fa31a07e5c4bd95b66d5a563ac222841d2
JordanCo1991 <68417100+JordanCo1991@users.noreply.github.com>
2023-05-08 22:55:52 +0300

    Dailychallengefinish2

--- Week2/day1/Dailychallenge/dc.py
+++ Week2/day1/Dailychallenge/dc.py
@@ -1,19 +1,32 @@
+text = input("Please enter a string that is 10 characters long: ")
 
+# Check the length of the string
+length = len(text)
+if length < 10:
+    print("String is too short,you have only", len(text),"characters")
+elif length > 10:
+    print("String is too long, you have", len(text),"characters")
+else:
+    print("String length is :" , len(text))
 
 
 text = input("Enter a string (10 characters): ")
 user_imput =len(text)
 
-if user_imput > 10:
+if len(text) > 10:
     print ("Your String is too long")
 
-else: user_imput < 10 
-print ("Your String is too short")
+elif len(text) < 10:
+    print ("Your String is too short")
+
+else:
+    print.len(text)
 
 
-string = input("Enter a string: ")
 
-for bla in range(1, len(string) + 1):
-    substring = string[:bla]
-    print(substring)
+# string = input("Enter a string: ")
+
+# for bla in range(1, len(string) + 1):
+#     substring = string[:bla]
+#     print(substring)
 
