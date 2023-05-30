e359822981573c68ed31af172159ecdab9d76530
JordanCo1991 <jordan.cohen.13@gmail.com>
2023-05-30 09:53:22 +0300

    del w5

--- Week5/mysite_top/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from polls.views import posts, profile
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/', posts, name='posts'),
-    path('profile/', profile)
-]
